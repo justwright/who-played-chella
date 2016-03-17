16e95dff9515be42d0fc9ef1e3e5b24c44b7d695
Justin Wright <justwright@me.com>
2016-03-17 00:24:31 -0700

    Added dependencies ember-cli-sass and ember-power-select

---
+++ app/components/artist-search.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    cities: ['Barcelona', 'London', 'New York', 'Porto'],
+    destination: 'London',
+    actions: {
+        chooseDestination(city) {
+            this.set('destination', city);
+        }
+    }
+});

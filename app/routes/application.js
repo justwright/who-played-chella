dc98ceb65e3027249e1e0e0dc9bc246d147a9719
Justin Wright <justwright@me.com>
2016-03-16 22:11:34 -0700

    Removing Ember data files for now; using AJAX

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
-		return this.store.findAll('artists');
+    model() {
+		//return this.store.findAll('artists');
+        return Ember.$.ajax({
+            type: 'GET',
+            url: 'http://localhost:4500/api/artists'
+        })
 	}
 });

1dfe08ebd2a3b8faca3c3737b8748908f072313a
Justin Wright <justwright@me.com>
2016-03-25 19:15:00 -0700

    Search is now functional and displays artist info

--- app/components/artist-search.js
+++ app/components/artist-search.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    cities: ['Barcelona', 'London', 'New York', 'Porto'],
-    destination: 'London',
     actions: {
-        chooseDestination(city) {
-            this.set('destination', city);
+        chooseArtist(selected) {
+            this.set('selectedArtist', selected);
         }
     }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-		//return this.store.findAll('artists');
         return Ember.$.ajax({
             type: 'GET',
             url: 'http://localhost:4500/api/artists'

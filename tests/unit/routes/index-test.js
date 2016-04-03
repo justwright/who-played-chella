5253ade3517ceb0beccf9d75b9d6d60303ac1e4f
Justin Wright <justwright@me.com>
2016-04-02 17:59:21 -0700

    Restructured app files by creating an index route underneath app.hbs

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:application', 'Unit | Route | application', {
+moduleFor('route:index', 'Unit | Route | index', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });

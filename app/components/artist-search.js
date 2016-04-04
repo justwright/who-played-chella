06622e6ee54599885b55157700b49b78d1846618
Justin Wright <justwright@me.com>
2016-04-04 00:05:07 -0700

    Added new branch for updating data flow

--- app/components/artist-search.js
+++ app/components/artist-search.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
     actions: {
         chooseArtist(selected) {
             this.set('selectedArtist', selected);
+        },
+        searchArtists(term) {
+            this.sendAction('queryArtist', term);
         }
     }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('artists');
+    },
+    actions: {
+        queryArtist(item) {
+            this.store.query('artists', {
+                equalTo: item
+            }).then(result => {
+                console.log(result);
+            })
+        }
     }
 });
